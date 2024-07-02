748697f26b187347026ba93b7a978c07d93a6314
AhmadSavaiz03 <ahmadsavaiz03@gmail.com>
2024-07-02 16:19:23 +0400

    implemented category

--- jobs-app-springboot/src/main/java/com/savaiz/jobs_app/dao/JobRepository.java
+++ jobs-app-springboot/src/main/java/com/savaiz/jobs_app/dao/JobRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+
     Page<Job> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+    Page<Job> findByCategory(@RequestParam("description") String description, Pageable pageable);
 }
