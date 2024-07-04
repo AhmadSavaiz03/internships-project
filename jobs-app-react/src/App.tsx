f4eba5867c7f1c7ed93b494f1556136601a7369f
AhmadSavaiz03 <ahmadsavaiz03@gmail.com>
2024-07-04 17:02:57 +0400

    okta implemented base 1

--- jobs-app-react/src/App.tsx
+++ jobs-app-react/src/App.tsx
@@ -1,37 +1,63 @@
-import "./App.css";
-import { Navbar } from "./layouts/Navbar-Footer/Navbar";
-import { Footer } from "./layouts/Navbar-Footer/Footer";
-import { HomePage } from "./layouts/HomePage/HomePage";
-import { SearchJobsPage } from "./layouts/SearchJobsPage/SearchJobsPage";
-import { Redirect, Route, Switch } from "react-router-dom";
-import { JobInformationPage } from "./layouts/JobInformationPage/JobInformationPage";
+import './App.css';
+import { Navbar } from './layouts/Navbar-Footer/Navbar';
+import { Footer } from './layouts/Navbar-Footer/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchJobsPage } from './layouts/SearchJobsPage/SearchJobsPage';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
+import { JobInformationPage } from './layouts/JobInformationPage/JobInformationPage';
+import { oktaConfig } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { LoginCallback, Security } from '@okta/okta-react';
+import LoginWidget from './auth/LoginWidget';
 
+const oktaAuth = new OktaAuth(oktaConfig);
 function App() {
+  const customAuthHandler = () => {
+    history.push('/login');
+  };
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
+
   return (
-    <div className="d-flex flex-column min-vh-1000">
-      <Navbar />
+    <div className='d-flex flex-column min-vh-100'>
+      <Security
+        oktaAuth={oktaAuth}
+        restoreOriginalUri={restoreOriginalUri}
+        onAuthRequired={customAuthHandler}>
+        <Navbar />
 
-      <div className="flex-grow-1">
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/home" />
-          </Route>
+        <div className='flex-grow-1'>
+          <Switch>
+            <Route path='/' exact>
+              <Redirect to='/home' />
+            </Route>
 
-          <Route path="/home">
-            <HomePage />
-          </Route>
+            <Route path='/home'>
+              <HomePage />
+            </Route>
 
-          <Route path="/search">
-            <SearchJobsPage />
-          </Route>
+            <Route path='/search'>
+              <SearchJobsPage />
+            </Route>
 
-          <Route path="/info/:jobId">
-            <JobInformationPage />
-          </Route>
-        </Switch>
-      </div>
+            <Route path='/info/:jobId'>
+              <JobInformationPage />
+            </Route>
+          </Switch>
+        </div>
 
-      <Footer />
+        <Route
+          path={'/login'}
+          render={() => <LoginWidget config={oktaConfig} />}
+        />
+        <Route path={'/logn/callback'} component={LoginCallback} />
+
+        <Footer />
+      </Security>
     </div>
   );
 }
